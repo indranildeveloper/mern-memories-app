2cfbaa87924097b1ec6d529231521f30fa343ab6
Indranil Halder <indranilhalder.dev@gmail.com>
2021-06-29 11:44:45 +0530

    Upload and Fetch Post Functionality added

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// Connect to database
+const CONNECTION_URL = "mongodb://localhost:27017/memories";
+const PORT = process.env.PORT || 7000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running at port ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
+
+// Routes
+app.use("/posts", postRoutes);
